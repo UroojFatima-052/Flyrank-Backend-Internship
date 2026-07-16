7336d5b53a4deb3b3f0634fcf0a0d25dec114574
Uroojfatima052 <uroojfatima6789@gmail.com.com>
2026-07-17 01:29:42 +0500

    Stage 4: full CRUD

--- Assignments/Week 02/main.py
+++ Assignments/Week 02/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
@@ -25,6 +25,7 @@ tasks = [
     }
 ]
 
+# Stage 01
 @app.get("/")
 def home():
     return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
@@ -33,6 +34,7 @@ def home():
 def status():
     return {"status" : "ok"}
 
+# Stage 02
 @app.get("/tasks")
 def all_tasks():
     return tasks
@@ -45,6 +47,8 @@ def specific_tasks(id : int):
         
     raise HTTPException(status_code=404, detail=f"Task {id} not found")
 
+
+# Stage 03
 class TaskInput(BaseModel):
     title : str
 
@@ -61,6 +65,45 @@ async def add_tasks(task: TaskInput):
     
     tasks.append(new_task)
     return JSONResponse(status_code=201, content=new_task)
+
+#Stage 04
+class TaskUpdate(BaseModel):
+    title : str = None
+    done : bool = None
+
+@app.put("/tasks/{id}")
+def update_tasks(id : int, task : TaskUpdate):
+    if task.title is None and task.done is None:
+        return JSONResponse(status_code=400, content={"error": "Data cannot be empty"})
+    
+    for item in tasks:
+        if item["id"] == id:
+            if task.title is not None:
+                item['title'] = task.title
+            if task.done is not None:
+                item['done'] = task.done
+
+            return JSONResponse(status_code=200, content=item)
+    
+    raise HTTPException(status_code=404, detail=f"Task {id} not found")
+
+
+@app.delete("/tasks/{id}")
+def delete_tasks(id : int):
+    for item in tasks:
+        if item["id"] == id:
+            tasks.remove(item)
+            return Response(status_code=204)
+        
+    raise HTTPException(status_code=404, detail=f"Task {id} not found")
+    
+    
+    
+
+
+
+
+
 
 
 
